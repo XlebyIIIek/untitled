ca8a593a1444fad5d81ec5021fb57be30653b69c
XlebyIIIek <137320012+XlebyIIIek@users.noreply.github.com>
2023-06-22 17:01:15 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,143 +1,121 @@
-import java.lang.annotation.ElementType;
 import java.util.Scanner;
 import java.io.IOException;
-
 public class Main {
-    public static boolean check_number(String number_1, String number_2, String sign) {
-        String[] rim_numbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        String[] rim_numbers_2 = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String input = scanner.nextLine();
+        String[] expression = input.split(" ");
+        check_expression(expression);
+    }
+    public static boolean check_expression(String[] expression){
+        //Проверим кол-во элементов в выражении
+        if(expression.length != 3){
+            return throw_exception("Выражение должно состоять из 3 элементов разделенных пробелами, пример a + b");
+        }
+        //Проверим знак выражения
+        String sign = expression[1];
+        String sings = "+-*/";
+        if (sings.indexOf(sign) == -1){
+            return throw_exception("Неверный знак, можно только +-*/");
+        }
+
+
+        //Проверяем числа (вдруг римские)
+        String[] rim_number = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] numbers_2 = {100, 90, 50, 40, 10, 9, 5, 4, 1};
-        Boolean f1 = false;
-        Boolean f2 = false;
-        int index_1 = 0;
-        int index_2 = 0;
-        int count = 0;
-        for (String number : rim_numbers) {
-            if (number_1.equals(number)) {
-                f1 = true;
-                index_1 = count;
+        String number_1 = expression[0];
+        String number_2 = expression[2];
+        int convert_number_1 = 0;
+        int convert_number_2 = 0;
+        for (int i = 0; i <rim_number.length; i++){
+            if(number_1.equals(rim_number[i]) ){
+                convert_number_1 = numbers[i];
             }
-            if (number_2.equals(number)) {
-                f2 = true;
-                index_2 = count;
+            if(number_2.equals(rim_number[i])){
+                convert_number_2 = numbers[i];
             }
-            count += 1;
         }
-        if (f1 && f2 && !sign.equals("-")) {
-            int answer = 0;
-            switch (sign) {
-                case "+":
-                    answer = numbers[index_1] + numbers[index_2];
-                    break;
-                case "-":
-                    answer = numbers[index_1] - numbers[index_2];
-                    break;
-                case "*":
-                    answer = numbers[index_1] * numbers[index_2];
-                    break;
-                case "/":
-                    answer = numbers[index_1] / numbers[index_2];
-                    break;
-            }
-            if (answer < 1){
-                try {
-                    throw new Exception();
-                } catch (Exception exception) {
-                    System.out.println("Результат вычисления римских чисел должен быть положительным числом");
-                    return true;
+        //Если римские от 0 до 10 и разность больше 0, то считаем ответ
+        if(convert_number_1 != 0 && convert_number_2 != 0){
+            if (check_numbers(convert_number_1, convert_number_2)){
+                if (convert_number_1 - convert_number_2 < 1 && sign.equals("-")){
+                    return throw_exception("В римской системе нет отрицательных чисел и 0");
                 }
-            }
-            int i = 0;
-            String answ = "";
-            for (int number: numbers_2){
-                while (answer - number >= 0){
-                    answer -= number;
-                    answ += rim_numbers_2[i];
-                }
-                i+=1;
-            }
-            System.out.println(answ);
-            return true;
-        }
-        if (f1 || f2){
-            try {
-                throw new Exception();
-            } catch (Exception exception) {
-                System.out.println("Либо оба числа римские, либо оба арабские должны быть");
+                calculate_rim_expression(convert_number_1, convert_number_2, sign);
                 return true;
             }
+            else {
+                throw_exception("Числа должны быть от 1 до 10");
+            }
+        }
+        //Проверяем нет ли случайно одного римского числа
+        if(convert_number_1 != 0 || convert_number_2 != 0){
+            return throw_exception("Оба числы должны быть римскими или арабскими");
+        }
+        //Проверяем являются ли числа целыми
+        try {
+            convert_number_1 = Integer.parseInt(number_1);
+            convert_number_2 = Integer.parseInt(number_2);
+        }catch (Exception exception){
+            System.out.println("Числа должны быть целыми");
+            return true;
+        }
+        //Проверяем числа чтобы были от 1 до 10
+        if (check_numbers(convert_number_1, convert_number_2)){
+            System.out.println(calculate_answer(convert_number_1,convert_number_1, sign));
+        }
+        else {
+            throw_exception("Числа должны быть от 1 до 10");
+        }
+
+
+
+        return false;
+    }
+    public static boolean throw_exception(String massage){
+        try {
+            throw new Exception();
+        }catch (Exception exception){
+            System.out.println(massage);
+            return true;
+        }
+    }
+    public static void calculate_rim_expression(int number_1, int number_2, String sign){
+        String[] rim_numbers = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] numbers = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int int_answer = calculate_answer(number_1, number_2, sign);
+        String rim_answer = "";
+        for (int i = 0; i < rim_numbers.length; i++){
+            while (int_answer - numbers[i] > -1){
+                int_answer -= numbers[i];
+                rim_answer += rim_numbers[i];
+            }
+        }
+        System.out.println(rim_answer);
+
+
+    }
+    public static int calculate_answer(int a, int b, String sign){
+        int answer = 0;
+        switch (sign){
+            case "+":
+                return a+b;
+            case "-":
+                return a-b;
+            case "*":
+                return a*b;
+            case "/":
+                return a/b;
+        }
+        return answer;
+    }
+    public static boolean check_numbers(int a, int b){
+        if (a > 0 && a < 11 && b > 0 && b < 11){
+            return true;
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Введите элементы математического выражения через пробел (a + b):");
-        Scanner scanner = new Scanner(System.in);
-        String[] expression = scanner.nextLine().split(" ");
-        int number_1 = 0;
-        int number_2 = 0;
-        //Проверяем выражение
-        if (expression.length != 3) {
-            try {
-                throw new Exception("Кол-во элементов больше чем ожидалось");
-            } catch (Exception exception) {
-                System.out.println("Кол-во элементов больше чем ожидалось");
-                return;
-            }
-        }
-        //Проверяем знак
-        String signs = "+-*/";
-        String sign = expression[1];
-        if (signs.indexOf(sign) == -1){
-            try {
-                throw new Exception();
-            } catch (Exception exception) {
-                System.out.println("Можно использовать знаки +-*/");
-                return;
-            }
-        }
-        //Проверяем наличие римских цифр
-        if (check_number(expression[0], expression[2], expression[1])) {
-            return;
-        }
-        //Пробуем преоброзовать в целое число
-        try {
-            number_1 = Integer.parseInt(expression[0]);
-        } catch (NumberFormatException exception) {
-            System.out.println(exception);
-            return;
-        }
-        try {
-            number_2 = Integer.parseInt(expression[2]);
-        } catch (NumberFormatException exception) {
-            System.out.println(exception);
-            return;
-        }
-        //Проверяем числа
-        if (number_1 < 1 || number_1 > 10 || number_2 < 1 || number_2 > 10) {
-            try {
-                throw new Exception();
-            } catch (Exception exception) {
-                System.out.println("Введенные числа должны быть от 1 до 10 включительно");
-                return;
-            }
 
-        }
-        //Смотрим знак и выполняем соответствующуюб операцию
-        switch (sign) {
-            case "+":
-                System.out.println(number_1 + number_2);
-                break;
-            case "-":
-                System.out.println(number_1 - number_2);
-                break;
-            case "*":
-                System.out.println(number_1 * number_2);
-                break;
-            case "/":
-                System.out.println(number_1 / number_2);
-                break;
-        }
-    }
 }
